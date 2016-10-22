d9483ed56a9e44737d36927af49b971e2d9d467a
son1cman <kmson1cman@gmail.com>
2016-10-22 07:58:20 -0600

    update

--- index.js
+++ index.js
@@ -132,6 +132,23 @@ function kittenMessage(recipientId, text) {
     if (values.length === 3 && values[0] === 'kitten') {
         if (Number(values[1]) > 0 && Number(values[2]) > 0) {
             
+         sendGenericMessage(recipientId);
+            
+            return true;
+        }
+    }
+    
+    return false;
+    
+};
+function kittenMessage_backup(recipientId, text) {
+    
+    text = text || "";
+    var values = text.split(' ');
+    
+    if (values.length === 3 && values[0] === 'kitten') {
+        if (Number(values[1]) > 0 && Number(values[2]) > 0) {
+            
             var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
             
             message = {
