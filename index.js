5255af062550706f7c48719354baee2772acca72
kevin martinez <kmson1cman@gmail.com>
2016-10-27 23:10:05 -0600

    update3

--- index.js
+++ index.js
@@ -28,14 +28,16 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-            if(event.message.text == 'Hola'){
-                sendMessage(event.sender.id, {text: "Hola, bienvenido a coexport. Escribe el producto o servicio que desees y yo podre asistirte"});
-            }
+
             
             if(!EcomMessage(event.sender.id, event.message.text)) {
             if (!kittenMessage(event.sender.id, event.message.text)) {
                 
+                                if(event.message.text == 'Hola'){
+                sendMessage(event.sender.id, {text: "Hola, bienvenido a coexport. Escribe el producto o servicio que desees y yo podre asistirte"});
+            }else{
                     sendMessage(event.sender.id, {text: "" + event.message.text});
+            }
                     //sendGenericMessage(event.sender.id);
                 
             }
