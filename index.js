6335bac3d878d8c73a2136c0bea07d4c5252a7e4
son1cman <kmson1cman@gmail.com>
2016-12-08 09:49:04 -0600

    typo

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.listen((process.env.PORT || 3000));
 app.get('/', function (req, res) {
     res.send('<html>' + '<body>' 
       + '<h2>Spectacular Mountain</h2>' +
-       '<img src="http://i.giphy.com/l2Sq5pd3utN3LgPqo.gif alt="Mountain View" style="width:304px;height:228px;">'+
+       '<img src="http://i.giphy.com/l2Sq5pd3utN3LgPqo.gif" alt="Mountain View" style="width:304px;height:228px;">'+
        '</body>' + '</html>');
     
 });
