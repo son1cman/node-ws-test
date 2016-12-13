64b576e422b98b40d32e7df220ee1513171e67dd
son1cman <kmson1cman@gmail.com>
2016-12-12 19:48:49 -0600

    u1

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ app.get('/AAA0B', function (req, res) {
            res.send('<html>' + '<body>' 
        +
        '<img src="http://i.imgur.com/7DBh3pu.jpg" alt="Mountain View" style="width:304px;height:228px;">'+
-       + '<p>Random meme 01 and insurance meme policy</p>'+
-       + '<p>Random meme 02 and insurance meme policy</p>'+
-       + '<p>Random insurance 03</p>'+
+       + 'Random meme 01 and insurance meme policy'+
+       + 'Random meme 02 and insurance meme policy'+
+       + 'Random insurance 03'+
        '</body>' + '</html>');
     xD++;
     console.log('Visitor number:' + xD + " at ip: " + req.headers['x-forwarded-for']);
